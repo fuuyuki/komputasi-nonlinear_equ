4004886e809d2917650772ac6e58170bbd062823
Muhammad Fikra Adzaky <fikraadzaky123@gmail.com>
2026-04-07 17:39:36 +0700

    add ipynb files

--- curve_fitting/leastSquareReg.py
+++ curve_fitting/leastSquareReg.py
@@ -1,16 +1,15 @@
 import numpy as np
 
-# x = np.array([0.24, 0.65, 0.95, 1.24, 1.73, 2.01, 2.23, 2.52])
-x = np.array([1, 2, 3])
-y = np.array([5.1, 5.9, 6.3])
+x = np.array([0.24, 0.65, 0.95, 1.24, 1.73, 2.01, 2.23, 2.52])
+y = np.array([0.23, -0.23, -1.1, -0.45, 0.27, 0.1, -0.29, 0.24])
 
 # Linear fit
 coeffs_linear = np.polyfit(x, y, 1)
 # Quadratic fit
-# coeffs_quad = np.polyfit(x, y, 2)
+coeffs_quad = np.polyfit(x, y, 2)
 # Cubic polynomial fit
-# coeffs_poly3 = np.polyfit(x, y, 3)
+coeffs_poly3 = np.polyfit(x, y, 3)
 
 print("Linear:", coeffs_linear)
-# print("Quadratic:", coeffs_quad)
-# print("Cubic:", coeffs_poly3)
+print("Quadratic:", coeffs_quad)
+print("Cubic:", coeffs_poly3)
